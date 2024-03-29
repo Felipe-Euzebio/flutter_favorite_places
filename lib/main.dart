156ca01f56a3d1705cdefb945998e1317d2a2d91
Felipe <99846344+Felipe-Euzebio@users.noreply.github.com>
2024-03-29 16:14:11 -0300

    Adding a Riverpod Provider (userPlacesProvider)

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
   seedColor: const Color.fromARGB(255, 102, 6, 247),
@@ -28,7 +30,9 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-      const MyApp(),
+    const ProviderScope(
+      child: MyApp(),
+    ),
   );
 }
 
